9b6d9c74f3ec10dd14e0abaf96ccc567a3968e15
Md: Ammad Ali <mdammad75@gmail.com>
2015-09-04 16:55:38 +0600

    Update and rename .gitignore to header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+	<?php
+							wp_list_categories( array(
+								'orderby'    => 'count',
+								'order'      => 'DESC',
+								'show_count' => 1,
+								'title_li'   => '',
+								'number'     => 10,
+							) );
+						?>
